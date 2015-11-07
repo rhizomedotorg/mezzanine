8b548f08ae0ba9c35428c191f8a5e75babb442c0
Matthew Conlen <mc@mathisonian.com>
2015-11-07 18:29:13 -0500

    override blog featured image

--- mezzanine/blog/models.py
+++ mezzanine/blog/models.py
@@ -24,9 +24,7 @@ class BlogPost(Displayable, Ownable, RichText, AdminThumbMixin):
                                          default=True)
     comments = CommentsField(verbose_name=_("Comments"))
     rating = RatingField(verbose_name=_("Rating"))
-    featured_image = FileField(verbose_name=_("Featured Image"),
-        upload_to=upload_to("blog.BlogPost.featured_image", "blog"),
-        format="Image", max_length=255, null=True, blank=True)
+    featured_image = models.ImageField(verbose_name=_("Featured Image"), upload_to=upload_to("blog.BlogPost.featured_image", "blog"), max_length=255, null=True, blank=True)
     related_posts = models.ManyToManyField("self",
                                  verbose_name=_("Related posts"), blank=True)
 
